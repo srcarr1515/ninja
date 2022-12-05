77463348cb43171a5687e9c2003a20df6fc85311
Shane Carr <=>
2022-12-05 06:10:16 -0500

    enemy actor and nav2d

---
+++ scenes/actors/enemy/Enemy.gd
@@ -0,0 +1,29 @@
+extends KinematicBody2D
+
+onready var nav_agent := $NavAgent
+onready var nav_timer := $NavTimer
+onready var sprite := $Sprite
+onready var hitbox := $HitBox
+onready var detectbox := $DetectBox
+onready var hurtbox := $HurtBox
+onready var state_machine := $StateMachine
+
+func _ready():
+	nav_agent.connect("velocity_computed", self, "_on_velocity_computed")
+
+func _on_velocity_computed(velocity):
+	pass
+
+func _on_NavTimer_timeout():
+	if GameData.player.global_position:
+		nav_agent.set_target_location(GameData.player.global_position)
+
+func _physics_process(delta):
+	if nav_agent.is_navigation_finished():
+		return
+	var target_pos = nav_agent.get_next_location()
+	var direction = global_position.direction_to(target_pos)
+	var velocity = direction * nav_agent.max_speed
+	move_and_slide(velocity)
+
+
--- scenes/actors/player/Player.gd
+++ scenes/actors/player/Player.gd
@@ -7,6 +7,7 @@ export var speed:= 500
 export var friction:= 0.01
 export var acceleration:= 0.1
 var new_angle
+var cur_side
 
 var slide_ct = 0
 
@@ -18,8 +19,6 @@ func _input(event):
 			new_angle = event.position.angle_to_point(first_touch)
 			target_position = get_end_pos_from_start_pos_and_angle(position, new_angle, 640)
 			slide_ct = 0
-			print(new_angle)
-			
 
 func get_inverted_pos(a:Vector2, b:Vector2, dist):
 	return a + ((a - b).normalized()) * dist
@@ -54,32 +53,19 @@ func _physics_process(delta):
 		for i in get_slide_count():
 			var collision = get_slide_collision(i)
 			side.push_front(get_collision_side(collision.position))
-			print(side)
+		
 		var can_move = false
-		if new_angle:
-			if (side.has("LEFT") || side.has("RIGHT")) && abs(new_angle) < 1 || abs(new_angle) > 3:
-				can_move = false
-			elif (side.has("UP") || side.has("DOWN")) && abs(new_angle) >= 1 && abs(new_angle) <= 2:
-				can_move = false
-			if (side.has("LEFT") || side.has("RIGHT")) && abs(new_angle) >= 1 && abs(new_angle) <= 2:
-				can_move = true
-				if (side.has("UP") || side.has("DOWN")) && abs(new_angle) >= 1 && abs(new_angle) <= 2:
-					can_move = true
-			elif (side.has("UP") || side.has("DOWN")) && abs(new_angle) < 1 || abs(new_angle) > 3:
-				can_move = true
-				if (side.has("LEFT") || side.has("RIGHT")) && abs(new_angle) >= 1 && abs(new_angle) <= 2:
-					can_move = true
-#		print(side)
+		var vert_move = abs(new_angle) >= 1 && abs(new_angle) <= 2
+		var horz_move = abs(new_angle) < 0.25 || abs(new_angle) > 3
+				
+		if vert_move:
+			can_move = (side.has("LEFT") || side.has("RIGHT")) && is_on_wall()
+		elif horz_move:
+			can_move = (side.has("UP") || side.has("DOWN")) && (is_on_floor() || is_on_ceiling())
+			
 		if position.distance_to(target_position) >= 5 && (slide_ct == 0 || can_move):
-			velocity = move_and_slide(velocity)
+			velocity = move_and_slide(velocity, Vector2.UP)
 			slide_ct = get_slide_count()
-
-#		if new_angle:
-#			if (side.has("LEFT") || side.has("RIGHT")) && abs(new_angle) < 1 || abs(new_angle) > 3:
-#				can_move = false
-#			elif (side.has("UP") || side.has("DOWN")) && abs(new_angle) >= 1 && abs(new_angle) <= 2:
-#				can_move = false
-#			elif (side.has("LEFT") || side.has("RIGHT")) && abs(new_angle) >= 1 && abs(new_angle) <= 2:
-#				can_move = true
-#			elif (side.has("UP") || side.has("DOWN")) && abs(new_angle) < 1 || abs(new_angle) > 3:
-#				can_move = true	
+		else:
+			cur_side = side
+			velocity = Vector2.ZERO
